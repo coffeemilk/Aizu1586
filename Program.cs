aa553728c4ac678649e6f9efdb61c8ce85705a79
coffeemilk <yutshige@googlemail.com>
2022-07-30 22:24:52 +0900

    BitArray but it does not fit

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
             var initialTiles = InitializeTiles();
             //太郎君は、 i 日目の朝に、左から axi 番目で上から ayi 番目のタイルを左上、 左から bxi 番目で上から byi 番目のタイルを右下にした長方形領域に存在しているタイルがすべて白いかどうかを確認する。 
             //もしすべてのタイルが白かった場合、それらのタイルをすべて黒く塗りつぶす。それ以外のときは何もしない。
-            var numberOfblackedTiles = PaintItBlackIfNecessary(initialTiles);
+            var numberOfblackedTiles = PaintItBlackIfNecessary(initialTiles.rectangleCoordinates, initialTiles.tiles);
             //N 日間のそれぞれの日において、その日の作業が終了した時点で黒く塗りつぶされたタイルが何枚あるかを出力せよ。
             ShowOutput(numberOfblackedTiles);
         }
@@ -30,21 +30,23 @@ namespace MyApp // Note: actual namespace depends on the project name.
             //Console.ReadKey();
         }
 
-        static  IReadOnlyCollection<ulong> PaintItBlackIfNecessary( IReadOnlyCollection<RectangleCoordinate> rectangleCoordinates)
+        static  IReadOnlyCollection<ulong> PaintItBlackIfNecessary( IReadOnlyCollection<RectangleCoordinate> rectangleCoordinates, CustomBitArray tiles)
         {
             var days = rectangleCoordinates.Count;
             var blackTiles = new List<ulong>(days);
-            var processedRectangleCoordinates = new List<RectangleCoordinate>(days);
+            //var processedRectangleCoordinates = new List<RectangleCoordinate>(days);
             ulong inTotalBlackTiles = 0;
 
             var spanRects = new Span<RectangleCoordinate>(rectangleCoordinates.ToArray());
             foreach(var spanRect in spanRects)
             {
-                var processedSpanRect = new Span<RectangleCoordinate>(processedRectangleCoordinates.ToArray());
-                if (!FindBlackTiles(processedSpanRect, spanRect))
+                //var processedSpanRect = new Span<RectangleCoordinate>(processedRectangleCoordinates.ToArray());
+                //if (!FindBlackTiles(processedSpanRect, spanRect))
+                if (!FindBlackTiles(tiles, spanRect))
                 {
-                    inTotalBlackTiles += PaintItBlack(spanRect);
-                    processedRectangleCoordinates.Add(spanRect);
+                    PaintItBlack(tiles, spanRect);
+                    inTotalBlackTiles += spanRect.ArraySize;
+                    //processedRectangleCoordinates.Add(spanRect);
                 }
 
                 blackTiles.Add(inTotalBlackTiles);
@@ -53,36 +55,55 @@ namespace MyApp // Note: actual namespace depends on the project name.
             return blackTiles;
         }
 
-        static bool FindBlackTiles(ReadOnlySpan<RectangleCoordinate> processedRectangleCoordinatesInBlack, RectangleCoordinate rectangleCoordinate)
-        //static bool FindBlackTiles(IReadOnlyCollection<RectangleCoordinate> processedRectangleCoordinatesInBlack, RectangleCoordinate rectangleCoordinate)
+        //static bool FindBlackTiles(ReadOnlySpan<RectangleCoordinate> processedRectangleCoordinatesInBlack, RectangleCoordinate rectangleCoordinate)
+        static bool FindBlackTiles(CustomBitArray tiles, RectangleCoordinate rectangleCoordinate)
         {
             bool result = false;
 
-            //  Parallel.ForEach(processedRectangleCoordinatesInBlack, new ParallelOptions { MaxDegreeOfParallelism = 2 }, (processedRectangleCoordinateInBlack, state) =>
+            for(uint xindex = rectangleCoordinate.Vertex1.x; xindex <= rectangleCoordinate.Vertex2.x; xindex++)
+            {
+                for(uint yindex = rectangleCoordinate.Vertex1.y; yindex <= rectangleCoordinate.Vertex2.y; yindex++)
+                {
+                    var tileColor = tiles[xindex-1, yindex-1];
+                    if (tileColor)
+                    {
+                        //PaintItWhite(tiles, rectangleCoordinate);
+                        return true;
+                    }
+                }
+            }
+            // foreach(var processedRectangleCoordinateInBlack in processedRectangleCoordinatesInBlack)
             // {
             //     if (IsOverlapped(processedRectangleCoordinateInBlack, rectangleCoordinate))
             //     {
-            //         result = true;
-            //         state.Stop();
+            //         return true;
             //     }
-            // });
-            foreach(var processedRectangleCoordinateInBlack in processedRectangleCoordinatesInBlack)
-            {
-                if (IsOverlapped(processedRectangleCoordinateInBlack, rectangleCoordinate))
-                {
-                    return true;
-                }
-            }
+            // }
+
 
             return result;
         }
 
-        private static ulong PaintItBlack(RectangleCoordinate rectangleCoordinate)
+        static void PaintItWhite(CustomBitArray tiles, RectangleCoordinate rectangleCoordinate)
         {
-            uint width = rectangleCoordinate.Vertex2.x - rectangleCoordinate.Vertex1.x + 1;
-            uint height = rectangleCoordinate.Vertex2.y - rectangleCoordinate.Vertex1.y + 1;
-            ulong arraySize = width * height;
-            return arraySize;
+            for(uint xindex = rectangleCoordinate.Vertex1.x; xindex <= rectangleCoordinate.Vertex2.x; xindex++)
+            {
+                for(uint yindex = rectangleCoordinate.Vertex1.y; yindex <= rectangleCoordinate.Vertex2.y; yindex++)
+                {
+                    tiles[xindex-1, yindex-1] = false;
+                }
+            }
+        }
+
+        static void PaintItBlack(CustomBitArray tiles, RectangleCoordinate rectangleCoordinate)
+        {
+            for(uint xindex = rectangleCoordinate.Vertex1.x; xindex <= rectangleCoordinate.Vertex2.x; xindex++)
+            {
+                for(uint yindex = rectangleCoordinate.Vertex1.y; yindex <= rectangleCoordinate.Vertex2.y; yindex++)
+                {
+                    tiles[xindex-1, yindex-1] = true;
+                }
+            }
         }
 
         static bool IsOverlapped(RectangleCoordinate processedRectangleCoordinate, RectangleCoordinate rectangleCoordinate)
@@ -106,7 +127,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
         }
 
 
-        static  IReadOnlyCollection<RectangleCoordinate> InitializeTiles()
+        static (IReadOnlyCollection<RectangleCoordinate> rectangleCoordinates, CustomBitArray tiles) InitializeTiles()
         {
             //入力形式
             //W H
@@ -120,10 +141,10 @@ namespace MyApp // Note: actual namespace depends on the project name.
             var parsedInput = ParseInputData(input);
             //var tiles = CreateTiles(parsedInput.W, parsedInput.H);
 
-            return parsedInput.rectangleCoordinates;
+            return (parsedInput.rectangleCoordinates, parsedInput.tiles);
         }
 
-        static (uint W, uint H, IReadOnlyCollection<RectangleCoordinate> rectangleCoordinates) ParseInputData( IReadOnlyCollection<string> input)
+        static (uint W, uint H, IReadOnlyCollection<RectangleCoordinate> rectangleCoordinates, CustomBitArray tiles) ParseInputData( IReadOnlyCollection<string> input)
         {
             var rectangleSize = input.ElementAt(0).Split(" ");
             uint W = uint.Parse(rectangleSize[1]);
@@ -140,7 +161,9 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 rectangleCoordinates.Add(new RectangleCoordinate(vertex1, vertex2));
             }
 
-            return (W, H, rectangleCoordinates);
+            var tiles = new CustomBitArray(W, H);
+
+            return (W, H, rectangleCoordinates, tiles);
         }
 
         static  IReadOnlyCollection<string> ReadInputData
@@ -187,14 +210,64 @@ namespace MyApp // Note: actual namespace depends on the project name.
         {
             Vertex1 = vertex1;
             Vertex2 = vertex2;
+            var width = vertex2.x - vertex1.x + 1;
+            var height = vertex2.y - vertex1.y + 1;
+            ArraySize = width * height;
         }
 
-        internal (uint x, uint y) Vertex1 {get; }
-        internal (uint x, uint y) Vertex2 {get; }
+        internal (uint x, uint y) Vertex1 { get; }
+        internal (uint x, uint y) Vertex2 { get; }
 
+        internal ulong ArraySize { get; }
     }
 
+    internal class CustomBitArray
+    {
+        internal ulong[] RowBitArray {get;}
 
+        internal CustomBitArray(uint width, uint height)
+        {
+            Width = width;
+            Height = height;
+            ulong logicalSize = (ulong) width * (ulong) height;
+            ulong size = logicalSize/(ulong)(sizeof(ulong)*8) + 1;
+            RowBitArray = new ulong[size];
+        }
 
+        internal uint Width {get;}
+        internal uint Height {get;}
+        internal bool this[uint x, uint y]
+        {
+            get
+            {
+                var offset = Width * y + x;
+                var yindex = offset/(sizeof(ulong)*8);
+                var xindex = offset%(sizeof(ulong)*8);
+                int shiftCount = (int)xindex;
+                var targetBit = GetBit(RowBitArray[yindex], shiftCount);
+                return targetBit;
+            }
+            set
+            {
+                var offset = Width * y + x;
+                var yindex = offset/(sizeof(ulong)*8);
+                var xindex = offset%(sizeof(ulong)*8);
+                int shiftCount = (int)xindex;
+                SetBit(ref RowBitArray[yindex], shiftCount, value);
+            }
+        }
 
+        private bool GetBit( ulong src, int bit)
+        {
+            ulong mask = (ulong) 0x8000000000000000 >> bit;
+            return (src & mask) == mask;
+
+        }
+        private void SetBit(ref ulong src, int bit, bool val)
+        {
+            ulong mask = (ulong) 0x8000000000000000 >> bit;
+            src = val ? src | mask : src & ~mask;
+        }
+        //internal Tile this[int xi, int yi] { get => this.RowTiles[yi * this.Width + xi]; set => this.RowTiles[yi * this.Width + xi] = value; }
+    }
 }
